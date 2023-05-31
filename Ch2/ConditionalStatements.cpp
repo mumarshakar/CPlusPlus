9a5a75da0f3e255910ef44dc375a3e61815330f7
Muhammad Umar <spaceumar@gmail.com>
2023-05-30 22:26:18 -0700

    Last commit on 30/05/2023

---
+++ Ch2/ConditionalStatements.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	// Let the number be 20 
+	
+	int time = 20;
+	string result = (time < 18) ? "Good Day" : "Good Evening ";
+	
+	cout << result;
+
+}
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
- int x = 0, y = 2;
- x = y * 4;
- cout << x << endl << y << endl;
- return 0;
-}
